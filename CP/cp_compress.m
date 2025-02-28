7273e71adf85ee6a753571018a37901d9a92b0e7
ivanak7899 <ivankris.math@pmf.hr>
2025-02-28 15:13:32 +0000

    compressed image size calc added

--- CP/cp_compress.m
+++ CP/cp_compress.m
@@ -5,12 +5,29 @@ img = imread('cat.jpg');
 img = im2double(img);
 [I1, I2, I3] = size(img);
 
+% Veličina originalne slike
+original_size = whos('img');
+original_size_MB = original_size.bytes / (1024 * 1024); % Pretvaranje u MB
+
 %% Pretvaranje slike u tenzor
 X = tensor(img);
 
 %% CP dekompozicija
-R = 50; % Rang kompresije (veći R = bolja rekonstrukcija)
+R = 500; % Rang kompresije (veći R = bolja rekonstrukcija)
 [P, Uinit, output] = cp_als(X, R, 'tol', 1e-6, 'maxiters', 50, 'printitn', 1);
+
+% Veličina faktorskih matrica nakon kompresije
+A_size = whos('P');
+A_bytes = numel(P.U{1}) * 8; % Svaki double zauzima 8 bajtova
+B_bytes = numel(P.U{2}) * 8;
+C_bytes = numel(P.U{3}) * 8;
+lambda_bytes = numel(P.lambda) * 8;
+
+% Ukupna veličina nakon kompresije (u MB)
+compressed_size_MB = (A_bytes + B_bytes + C_bytes + lambda_bytes) / (1024 * 1024);
+
+% Faktor kompresije
+compression_ratio = original_size_MB / compressed_size_MB;
 
 %% Rekonstrukcija slike iz CP dekompozicije
 X_reconstructed = full(P);
